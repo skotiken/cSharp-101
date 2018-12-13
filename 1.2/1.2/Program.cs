dae50908d05c28e60343e36097f1fa944763e7d8
Dmitrijs Fedorenko <dmitrijsfedorenko@MacBook-Pro-Dmitrijs.local>
2018-12-13 21:26:27 +0200

    project last

---
+++ 1.2/1.2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Application
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter number");
+            String entered = Console.ReadLine();
+
+            int number1 = Convert.ToInt16(entered);
+
+            Console.WriteLine("Please enter second number");
+            String entered2 = Console.ReadLine();
+
+            int number2 = Convert.ToInt16(entered2);
+
+
+            Console.WriteLine("What do you want to do?");
+
+            String vards = Console.ReadLine();
+            
+            int sum = number1 + number2;
+            int sub = number1 - number2;
+            if (vards == "add")
+            {
+                Console.WriteLine(sum);
+            }
+            else if (vards == "subtract")
+            {
+                Console.WriteLine(sub);
+            }
+            else
+            {
+                Console.WriteLine("404");
+                }
+
+                Console.ReadLine();
+
+
+            }
+        }
+    }
+
+
