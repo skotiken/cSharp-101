cde53155fe6e923421563d4349abb2511183c43d
Dmitrijs Fedorenko <dmitrijsfedorenko@MacBook-Pro-Dmitrijs.local>
2018-12-13 19:43:57 +0200

    Project created and first task

---
+++ 1.1/1.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Application
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter your name");
+
+
+            String name = Console.ReadLine();
+
+           
+
+            Console.WriteLine("Welcome " + name);
+
+            Console.ReadLine();
+
+
+            //int, double, String, bool...
+
+        }
+    }
+}
